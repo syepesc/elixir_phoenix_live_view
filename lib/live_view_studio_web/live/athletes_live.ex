5153fcb4e86c77fb76b23656db164a19ed6a65b3
Santiago Yepes <syepesc@hotmail.com>
2023-11-13 10:47:57 -0500

    Update /athletes logic to make use of filters and stop caching full list of atheltes

--- lib/live_view_studio_web/live/athletes_live.ex
+++ lib/live_view_studio_web/live/athletes_live.ex
@@ -10,14 +10,14 @@ defmodule LiveViewStudioWeb.AthletesLive do
         athletes: Athletes.list_athletes()
       )
 
-    {:ok, socket}
+    {:ok, socket, temporary_assigns: [athletes: []]}
   end
 
   def render(assigns) do
     ~H"""
     <h1>Athletes</h1>
     <div id="athletes">
-      <form>
+      <form phx-change="filter">
         <div class="filters">
           <select name="sport">
             <%= Phoenix.HTML.Form.options_for_select(
@@ -71,5 +71,13 @@ defmodule LiveViewStudioWeb.AthletesLive do
       Competing: :competing,
       Resting: :resting
     ]
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("filter", %{"sport" => sport, "status" => status}, socket) do
+    filter = %{sport: sport, status: status}
+    athletes = Athletes.list_athletes(filter)
+
+    {:noreply, assign(socket, athletes: athletes, filter: filter)}
   end
 end
