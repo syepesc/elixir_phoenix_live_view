1dd7538f43047eaf4fd4763e3e1886081035958d
Santiago Yepes <santiagoyepescarrera@gmail.com>
2023-11-13 15:41:48 -0500

    Extrct athlete and filter_form component form html

--- lib/live_view_studio_web/live/athletes_live.ex
+++ lib/live_view_studio_web/live/athletes_live.ex
@@ -17,39 +17,10 @@ defmodule LiveViewStudioWeb.AthletesLive do
     ~H"""
     <h1>Athletes</h1>
     <div id="athletes">
-      <form phx-change="filter">
-        <div class="filters">
-          <select name="sport">
-            <%= Phoenix.HTML.Form.options_for_select(
-              sport_options(),
-              @filter.sport
-            ) %>
-          </select>
-          <select name="status">
-            <%= Phoenix.HTML.Form.options_for_select(
-              status_options(),
-              @filter.status
-            ) %>
-          </select>
-        </div>
-      </form>
+      <.filter_form filter={@filter} />
+
       <div class="athletes">
-        <div :for={athlete <- @athletes} class="athlete">
-          <div class="emoji">
-            <%= athlete.emoji %>
-          </div>
-          <div class="name">
-            <%= athlete.name %>
-          </div>
-          <div class="details">
-            <span class="sport">
-              <%= athlete.sport %>
-            </span>
-            <span class="status">
-              <%= athlete.status %>
-            </span>
-          </div>
-        </div>
+        <.athlete :for={athlete <- @athletes} athlete={athlete} />
       </div>
     </div>
     """
@@ -73,7 +44,52 @@ defmodule LiveViewStudioWeb.AthletesLive do
     ]
   end
 
-  @impl Phoenix.LiveView
+  attr :filter, :map, required: true
+
+  def filter_form(assigns) do
+    ~H"""
+    <form phx-change="filter">
+      <div class="filters">
+        <select name="sport">
+          <%= Phoenix.HTML.Form.options_for_select(
+            sport_options(),
+            @filter.sport
+          ) %>
+        </select>
+        <select name="status">
+          <%= Phoenix.HTML.Form.options_for_select(
+            status_options(),
+            @filter.status
+          ) %>
+        </select>
+      </div>
+    </form>
+    """
+  end
+
+  attr :athlete, LiveViewStudio.Athletes.Athlete, required: true
+
+  def athlete(assigns) do
+    ~H"""
+    <div class="athlete">
+      <div class="emoji">
+        <%= @athlete.emoji %>
+      </div>
+      <div class="name">
+        <%= @athlete.name %>
+      </div>
+      <div class="details">
+        <span class="sport">
+          <%= @athlete.sport %>
+        </span>
+        <span class="status">
+          <%= @athlete.status %>
+        </span>
+      </div>
+    </div>
+    """
+  end
+
   def handle_event("filter", %{"sport" => sport, "status" => status}, socket) do
     filter = %{sport: sport, status: status}
     athletes = Athletes.list_athletes(filter)
