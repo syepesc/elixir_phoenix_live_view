94e9cb7bb587043c5c65a531eda9f2f954667078
Santiago Yepes <syepesc@hotmail.com>
2023-11-24 15:46:32 -0500

    Add /pizza-orders logic

--- lib/live_view_studio_web/live/pizza_orders_live.ex
+++ lib/live_view_studio_web/live/pizza_orders_live.ex
@@ -5,11 +5,72 @@ defmodule LiveViewStudioWeb.PizzaOrdersLive do
   import Number.Currency
 
   def mount(_params, _session, socket) do
+    {:ok, socket, temporary_assigns: [pizza_orders: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = validate_sort_by(params)
+    sort_order = validate_sort_order(params)
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    orders = PizzaOrders.list_pizza_orders(options)
+
     socket =
       assign(socket,
-        pizza_orders: PizzaOrders.list_pizza_orders()
+        pizza_orders: orders,
+        options: options
       )
 
-    {:ok, socket}
+    {:noreply, socket}
   end
+
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+  slot :inner_block, required: true
+
+  def sort_button(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/pizza-orders?#{%{sort_by: @sort_by, sort_order: flip_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+      <%= sort_indicator(@sort_by, @options) %>
+    </.link>
+    """
+  end
+
+  defp validate_sort_by(%{"sort_by" => sort_by})
+       when sort_by in ~w(id size style topping_1 topping_2 price) do
+    String.to_existing_atom(sort_by)
+  end
+
+  defp validate_sort_by(_params), do: :id
+
+  defp validate_sort_order(%{"sort_order" => sort_order})
+       when sort_order in ~w(asc desc) do
+    String.to_existing_atom(sort_order)
+  end
+
+  defp validate_sort_order(_params), do: :asc
+
+  defp flip_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
+  end
+
+  defp sort_indicator(column, %{sort_by: sort_by, sort_order: sort_order})
+       when column == sort_by do
+    case sort_order do
+      :asc -> "👆"
+      :desc -> "👇"
+    end
+  end
+
+  defp sort_indicator(_, _), do: ""
 end
