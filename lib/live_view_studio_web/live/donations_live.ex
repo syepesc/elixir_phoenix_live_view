793578d6dff3d7dae6295ef1d6798e51b358b258
Santiago Yepes <syepesc@hotmail.com>
2023-11-24 14:20:26 -0500

    Add filters with icons to  /donations page

--- lib/live_view_studio_web/live/donations_live.ex
+++ lib/live_view_studio_web/live/donations_live.ex
@@ -4,13 +4,58 @@ defmodule LiveViewStudioWeb.DonationsLive do
   alias LiveViewStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
+    {:ok, socket, temporary_assigns: [donations: []]}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = String.to_atom(params["sort_by"] || "id")
+    sort_order = String.to_atom(params["sort_order"] || "asc")
+
+    options = %{
+      sort_by: sort_by,
+      sort_order: sort_order
+    }
+
+    donations = Donations.list_donations(options)
 
     socket =
       assign(socket,
-        donations: donations
+        donations: donations,
+        options: options
       )
 
-    {:ok, socket}
+    {:noreply, socket}
   end
+
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+  slot :inner_block, required: true
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/donations?#{%{sort_by: @sort_by, sort_order: flip_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+      <%= sort_indicator(@sort_by, @options) %>
+    </.link>
+    """
+  end
+
+  defp flip_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
+  end
+
+  defp sort_indicator(column, %{sort_by: sort_by, sort_order: sort_order})
+       when column == sort_by do
+    case sort_order do
+      :asc -> "👆"
+      :desc -> "👇"
+    end
+  end
+
+  defp sort_indicator(_, _), do: ""
 end
