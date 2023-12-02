2dbfffb83c7da36490f5ebc9d94767a047e9b38f
Santiago Yepes <syepesc@hotmail.com>
2023-12-02 13:49:03 -0500

    Add first form in /volunteers

--- lib/live_view_studio_web/live/volunteers_live.ex
+++ lib/live_view_studio_web/live/volunteers_live.ex
@@ -2,14 +2,18 @@ defmodule LiveViewStudioWeb.VolunteersLive do
   use LiveViewStudioWeb, :live_view
 
   alias LiveViewStudio.Volunteers
+  alias LiveViewStudio.Volunteers.Volunteer
 
   def mount(_params, _session, socket) do
     volunteers = Volunteers.list_volunteers()
 
+    # create empty changeset to pass to initial form values
+    changeset = Volunteers.change_volunteer(%Volunteer{})
+
     socket =
-      assign(socket,
-        volunteers: volunteers
-      )
+      socket
+      |> assign(volunteers: volunteers)
+      |> assign(form: to_form(changeset))
 
     {:ok, socket}
   end
@@ -18,6 +22,21 @@ defmodule LiveViewStudioWeb.VolunteersLive do
     ~H"""
     <h1>Volunteer Check-In</h1>
     <div id="volunteer-checkin">
+      <.form for={@form} phx-submit="save">
+        <.input field={@form[:name]} placeholder="Name" autocomplete="off" />
+        <.input
+          field={@form[:phone]}
+          type="tel"
+          placeholder="Phone"
+          autocomplete="off"
+        />
+        <.button phx-disable-with="Saving...">
+          Check In
+        </.button>
+      </.form>
+
+      <pre><%#= inspect(@form, pretty: true) %></pre>
+
       <div
         :for={volunteer <- @volunteers}
         class={"volunteer #{if volunteer.checked_out, do: "out"}"}
@@ -36,5 +55,26 @@ defmodule LiveViewStudioWeb.VolunteersLive do
       </div>
     </div>
     """
+  end
+
+  def handle_event("save", %{"volunteer" => volunteer_params}, socket) do
+    case Volunteers.create_volunteer(volunteer_params) do
+      {:ok, volunteer} ->
+        socket =
+          update(
+            socket,
+            :volunteers,
+            fn volunteers -> [volunteer | volunteers] end
+          )
+
+        # empty form to restart the UI
+        changeset = Volunteers.change_volunteer(%Volunteer{})
+        socket = put_flash(socket, :info, "Volunteer successfully checked in!")
+
+        {:noreply, assign(socket, form: to_form(changeset))}
+
+      {:error, changeset} ->
+        {:noreply, assign(socket, form: to_form(changeset))}
+    end
   end
 end
