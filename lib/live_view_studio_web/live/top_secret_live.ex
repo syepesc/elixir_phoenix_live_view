c71c05f722639b438603d4eda974a6167b241d46
Santiago Yepes <syepesc@hotmail.com>
2024-03-02 11:31:48 -0500

    Format string UI in /top-secret

--- lib/live_view_studio_web/live/top_secret_live.ex
+++ lib/live_view_studio_web/live/top_secret_live.ex
@@ -12,12 +12,18 @@ defmodule LiveViewStudioWeb.TopSecretLive do
       <div class="mission">
         <h1>Top Secret</h1>
         <h2>Your Mission</h2>
-        <h3>(spy number here)</h3>
+        <h3><%= pad_user_id(@current_user) %></h3>
         <p>
           Storm the castle and capture 3 bottles of Elixir.
         </p>
       </div>
     </div>
     """
+  end
+
+  defp pad_user_id(user) do
+    user.id
+    |> Integer.to_string()
+    |> String.pad_leading(3, "0")
   end
 end
